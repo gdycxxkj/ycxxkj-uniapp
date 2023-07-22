24b9dfdcfe6c7db41f023e7fcc14d8def4e667de
lzj500 <lzj500@qq.com>
2023-07-22 19:10:34 +0800

    删除test

--- test2.ts
+++
@@ -1,9 +0,0 @@
-
-import { Storage, Reg } from './src/index';
-// import Storage from "./libs/Storage";
-// const Storage=require("./libs/Storage");
-// const result = Storage.getPrifix();
-// let result2 = Storage.getPrifix();
-// console.log("result", result);
-// console.log("result2", result2);
-console.log("hello");
